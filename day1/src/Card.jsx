5bad88b1bc42ac7598fe9d9170c5538854cbb5c8
Ayub Shrestha <ayubsht1@gmail.com>
2025-02-20 16:03:45 +0545

    card

--- day1/src/Card.jsx
+++ day1/src/Card.jsx
@@ -2,9 +2,9 @@ import profilePic from './assets/ayub.png'
 function Card(){
     return(
         <div className="card">
-            <img src={profilePic} alt="profile picture"></img>
-            <h2>Ayub Code</h2>
-            <p>Full Stack Developer</p>
+            <img className="card-image" src={profilePic} alt="profile picture"></img>
+            <h2 className="card-title">Ayub Code</h2>
+            <p className="card-text">Full Stack Developer</p>
         </div>
     );
 }
