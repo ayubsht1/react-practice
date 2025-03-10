30e60b667743a46505839f35f14cd9b756dc10ce
Ayub Shrestha <ayubsht1@gmail.com>
2025-03-10 12:40:11 +0545

    day 14 useEffect

---
+++ day14/src/App.jsx
@@ -0,0 +1,12 @@
+import MyComponent from "./Mycomponent.jsx"
+import MyComponent2 from "./MyComponent2.jsx"
+
+function App() {
+  return (
+    <>
+    <MyComponent2/>
+    </>
+  )
+}
+
+export default App
